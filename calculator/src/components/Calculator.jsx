bbc88a48a2e450714ae5ed4a0b10323b0442f942
abdulqayoom77 <abdulqayoomaijaz77@gmail.com>
2025-08-31 22:07:45 +0500

    changes

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -4,7 +4,7 @@ export const Calculator = ({ calculatorButtons }) => {
   return (
     <div className="calculator">
       <input type="number" placeholder="Enter Value to calculate" id="input" />
-      <p></p>
+      <p>Calculator</p>
       <Buttons calculatorButtons={calculatorButtons} />
     </div>
   );
