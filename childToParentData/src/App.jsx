d4f3957cd859e49851fdc1acae7744d17ef907ad
abdulqayoom77 <abdulqayoomaijaz77@gmail.com>
2025-09-01 21:21:20 +0500

    child to parent

--- childToParentData/src/App.jsx
+++ childToParentData/src/App.jsx
@@ -9,21 +9,23 @@ const App = () => {
   // const items = ["Apple", "mango", "banana"];
   let h1 = "Food Items List";
   const [items, setItems] = useState(["Apple", "mango", "banana"]);
+  const [inputValue, setInputValue] = useState("")
 
   const onKeyDown = (e) => {
     if(e.key == "Enter"){
-      setItems([...items, valFromInputComponent]);
-
+      setItems([...items, inputValue]);
+      setInputValue("")
     }
   }
  
   const getInputValue = (valFromInputComponent) => {
+    setInputValue(valFromInputComponent)
   };
 
   return (
     <Container>
       <Heading h1={h1} />
-      <Input getInputValue={getInputValue} onKeyDown={onkeydown} />
+      <Input getInputValue={getInputValue} value={inputValue} onKeyDown={onKeyDown} />
       {items.length == 0 ? <EmptyMessage /> : <FoodItems items={items} />}
     </Container>
   );
--- childToParentData/src/components/Input.jsx
+++ childToParentData/src/components/Input.jsx
@@ -1,6 +1,6 @@
-const Input = ({getInputValue, onKeyDown}) => {
+const Input = ({getInputValue, onKeyDown, value}) => {
   return (
-    <input type="text" placeholder='Enter Food Item Name' className="p-3 border-1 border-black rounded-md w-76" onChange={(event)=> getInputValue(event.target.value)}  onKeyDown={(e) => onKeyDown(e)} />
+    <input type="text" placeholder='Enter Food Item Name' className="p-3 border-1 border-black rounded-md w-76" onChange={(event)=> getInputValue(event.target.value)}  onKeyDown={(e) => onKeyDown(e)} value={value} />
   )
 }
 
