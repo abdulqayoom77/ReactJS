d36bebb921af8508370de83380fb4653270f4969
abdulqayoom77 <abdulqayoomaijaz77@gmail.com>
2025-09-12 21:49:34 +0500

    input Validation

--- inputValidation/src/App.jsx
+++ inputValidation/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useRef } from 'react'
 
 const App = () => {
+  const inputRef = useRef(null)
+  const errorRef = useRef(null)
+
+  const handleValidation = () => {
+    const inputValue = inputRef.current.value;
+    if(inputValue.trim() === ""){
+      errorRef.current.style.display = 'block'
+    }else{
+      errorRef.current.style.display = ''
+    }
+  }
+  console.log("object");
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Input Validation (error message)</h1>
+      <input ref={inputRef} type="text" placeholder='Type Something' />
+      <div ref={errorRef} style={{color:'red', display: 'none'}} >Please Enter a Valid Input</div>
+    </div>
   )
 }
 
