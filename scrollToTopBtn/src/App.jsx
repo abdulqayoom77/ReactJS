0f6857c1745f932d37e36e60a1b9c0589e4c5518
abdulqayoom77 <abdulqayoomaijaz77@gmail.com>
2025-09-13 18:04:24 +0500

    scroll to top useEffect

--- scrollToTopBtn/src/App.jsx
+++ scrollToTopBtn/src/App.jsx
@@ -1,24 +1,67 @@
-import React, { useRef } from "react";
+// import React, { useRef } from "react";
+
+// const App = () => {
+//   const scrollRef = useRef(null);
+//   const scrollToTop = () => {
+//     scrollRef.current.scrollTo({ top: 0, behaviour: "smooth" });
+//   };
+//   return (
+//     <div  ref={scrollRef} className="h-[100vh] overflow-y-scroll p-[20px]">
+//       <div className="h-[300vh] p-[20px]">
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+//         <button
+//           style={{ position: "fixed", bottom: "20px" }}
+//           onClick={scrollToTop}
+//         >
+//           Scroll to top
+//         </button>
+//       </div>
+//       <div className="h-[200vh]">
+//         abc
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const scrollRef = useRef(null);
+  const [visible, setVisible] = useState(false);
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const scrollToTop = () => {
-    scrollRef.current.scrollTo({ top: 0, behaviour: "smooth" });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
-    <div  ref={scrollRef} className="h-[100vh] overflow-y-scroll p-[20px]">
-      <div className="h-[300vh] p-[20px]">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+    <div className="h-[300vh]">
+      {visible && (
         <button
-          style={{ position: "fixed", bottom: "20px" }}
           onClick={scrollToTop}
+          style={{ position: "fixed", bottom: "20px", right: "20px" }}
+          ref={buttonRef}
         >
-          Scroll to top
+          Scroll To Top
         </button>
-      </div>
-      <div className="h-[200vh]">
-        abc
-      </div>
+      )}
     </div>
   );
 };
